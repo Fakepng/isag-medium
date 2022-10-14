b6b808c118b74db76ce584ea4beaada17fb2c21e
1tpp <contact@1tpp.com>
2022-10-15 00:43:37 +0700

    chore: change color dark, light theme

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,29 +1,32 @@
 import Link from "next/link";
-const isProd = process.env.NODE_ENV === "production";
 
 const Card = ({ post }) => {
   return (
     <Link href={`/blog/${post.slug}`}>
-      <div className="flex dark:bg-slate-400 shadow-md bg-red-100  mb-10 rounded-lg p-6 justify-between cursor-pointer">
-        <div className="flex flex-col justify-between pr-8">
-          <div>
-            <h1 className="text-xl font-semibold">{post.frontMatter.title}</h1>
-            <p className="font-medium py-2">{post.frontMatter.description}</p>
+      <div className="flex justify-between bg-neutral-50 dark:bg-neutral-800 shadow-md rounded-lg p-6 cursor-pointer overflow-hidden">
+        <div className="flex flex-col justify-between space-y-2">
+          <div className="space-y-1">
+            <h2 className="text:md lg:text-xl font-medium leading-8 tracking-tight">{post.frontMatter.title}</h2>
+            <p className="font-light text-sm hidden md:block max-w-none lg:block">{post.frontMatter.description}</p>
           </div>
           <div className="flex">
-            <p className="bg-black mr-2 py-1 text-xs rounded-xl border-0 px-4 text-white">
+            <p className="bg-neutral-700 mr-2 py-1 text-xs rounded-xl border-0 px-4 text-neutral-100 ">
               {post.frontMatter.tags[0]}
             </p>
-            <p className="text-xs py-1">{post.frontMatter.date}</p>
+            <p className="text-xs py-1 font-lignt">{post.frontMatter.date}</p>
           </div>
         </div>
-        <div className="max-w-[250px]">
-          <img
-            className="w-full"
-            src={`${isProd ? process.env.BASE_PATH : ""}/static/images/posts/${post.slug}${post.frontMatter.thumbnailUrl}`}
-            alt=""
-          />
-        </div>
+        {
+          post.frontMatter.thumbnailUrl ? (
+
+            <img
+              className="max-w-[250px] ml-8 object-cover object-center hidden md:block lg:block"
+              src={`${process.env.NODE_ENV === "production" ? process.env.ASSETS : ""}/static/images/posts/${post.slug}${post.frontMatter.thumbnailUrl}`}
+              alt=""
+            />
+
+          ) : ""
+        }
       </div>
     </Link>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
+        <body>
           <Main />
           <NextScript />
         </body>
